44fa0bf56bace6aa3a8d48df4bf611b4a87066b7
Tayo <omotayo_dnl@yahoo.com>
2026-08-31 18:12:30 -0500

    removed route from the import to fix error of it being defined but not being used

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import CurrentUserContext from "../../context/CurrentUserContext";
 
 function ProtectedRoute({ children }) {
